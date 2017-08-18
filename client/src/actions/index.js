5202ff7984b7e974e24a1480875fdf58115d1e0f
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-17 17:18:34 -0700

    add stripe at server side

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,12 +3,12 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 const fetchUser_v0 = () => {
-  const request = axios.get('api/current_user');
+  const request = axios.get("api/current_user");
 
   return {
     type: FETCH_USER,
     payload: request
-  }
+  };
 };
 
 export const fetchUser_v1 = () => {
@@ -30,4 +30,7 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
- 
+export const handleToken = token =>  async dispatch => {
+  const res = await axios.post("api/stripe", token);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import StripCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+//
+import * as actions from "../actions";
 
 class Payments extends Component {
   render() {
@@ -8,7 +11,7 @@ class Payments extends Component {
         name="React-feedback"
         description="$5 from 5"
         amount={10}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add Credits</button>
@@ -17,4 +20,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
